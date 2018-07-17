248a2af940e56f8d656fef571e88bb0f4420dec2
Sanchit <ansifers@gmail.com>
2018-07-17 22:01:34 +0530

    Comments Updated

---
+++ app/src/main/java/com/example/urldataretriever/imagelibrary/RetrieveImage.java
@@ -0,0 +1,63 @@
+package com.example.urldataretriever.imagelibrary;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.app.LoaderManager;
+import android.content.Loader;
+
+import com.example.urldataretriever.BitmapWithIndex;
+
+public class RetrieveImage {
+
+    private Activity mContext;
+    private String imageURL;
+    public LoaderManager loaderManager;
+    private int index;
+    private RetrieveImageListener listener;
+
+
+    public RetrieveImage(Activity context, String imageURL, int index) {
+        this.listener = null;
+        mContext = context;
+        this.imageURL = imageURL;
+        this.index = index;
+        getImage();
+    }
+
+    private void getImage() {
+        loaderManager = mContext.getLoaderManager();
+        LoaderManager.LoaderCallbacks<BitmapWithIndex> bitmapWithIndexLoaderCallbacks = new LoaderManager.LoaderCallbacks<BitmapWithIndex>() {
+            @Override
+            public Loader<BitmapWithIndex> onCreateLoader(int i, Bundle bundle) {
+                return new ImageLoader(mContext, imageURL, index-1);
+            }
+
+            @Override
+            public void onLoadFinished(Loader<BitmapWithIndex> loader, BitmapWithIndex bitmapWithIndex) {
+                if(bitmapWithIndex != null) {
+                    listener.onDataLoaded(bitmapWithIndex.getBitmap(), bitmapWithIndex.getI());
+                }
+            }
+
+            @Override
+            public void onLoaderReset(Loader<BitmapWithIndex> loader) {
+
+            }
+        };
+
+        loaderManager.initLoader(index, null, bitmapWithIndexLoaderCallbacks);
+    }
+
+    public void setRetrieveImageListener(RetrieveImageListener retrieveImageListener) {
+        this.listener = retrieveImageListener;
+    }
+
+
+
+    public interface RetrieveImageListener {
+        public void onDataLoaded(Bitmap bitmap, int index);
+    }
+
+
+}
